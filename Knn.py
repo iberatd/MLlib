773cfd8a5ea10df166f964e85d7f7c3f191e0f71
iberatd <beratduzenli.01@gmail.com>
2023-07-08 22:45:31 +0300

    Knn Regression

--- Knn.py
+++ Knn.py
@@ -27,3 +27,26 @@ class KnnClassifier:
             results.append([np.argmax(np.bincount(n_labels[:,i]))])
 
         return np.array(results)
+    
+class KnnRegressor:
+    def __init__(self, k):
+        self.data=0
+        self.values=[]
+        self.k=k
+
+
+    def fit(self, X, y):
+
+        self.data=X
+        self.values=np.array(y)
+
+    def predict(self, X):
+        dist = cdist(self.data, X)
+
+        neighbours = np.argsort(dist, axis=0)[:self.k, :]
+
+        n_values = self.values[neighbours]
+
+        return n_values.mean(axis=0)
+    
+    
